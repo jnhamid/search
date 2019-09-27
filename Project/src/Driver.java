7c6c821dd440d313b766294989837391af7588f7
Sophie Engle <sjengle@cs.usfca.edu>
2019-09-27 12:12:33 -0700

    Resubmit for code review.

--- Project/src/Driver.java
+++ Project/src/Driver.java
@@ -7,6 +7,42 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+/*
+ * TODO Clean up any unnecessary System.out.println(...)
+ */
+
+// TODO Delete old TODO comments
+
+/*
+ * TODO Fix the Javadoc warnings...
+Javadoc: The method asNestedObject(TreeMap<String,ArrayList<Integer>>, Writer, int) in the type SimpleJsonWriter is not applicable for the arguments (Map, Writer, int)	SimpleJsonWriter.java	/Project/src	line 266	Java Problem
+Javadoc: The method asNestedObject(TreeMap<String,ArrayList<Integer>>, Writer, int) in the type SimpleJsonWriter is not applicable for the arguments (Map, Writer, int)	SimpleJsonWriter.java	/Project/src	line 315	Java Problem
+ */
+
+/*
+ * TODO Fix the formatting (and use of blanks lines) and remove commented out code.
+ */
+
+/*
+ * TODO Exception handling
+ * Handle exceptions where you interact with the user. In this project, that means
+ * throw all your exceptions to Driver.main, but Driver.main must catch them.
+ * 
+ * All output should be user-friendly and informative.
+ * 
+ * 1) Stack traces are not user-friendly.
+ * 2) Messages like "Error happened." not informative.
+ */
+
+/*
+ * TODO Class design... Driver is the only programmer-specific class that does not
+ * get shared with other developers. So any useful code should in another class.
+ * (Like... traversing the directory.)
+ * 
+ * InvertedIndexBuilder class that has all of the directory traversing and file parsing
+ * code related to building an index.
+ */
+
 /**
  * Class responsible for running this project based on the provided command-line
  * arguments. See the README for details.
@@ -63,8 +99,24 @@ public class Driver {
 			SimpleJsonWriter.asDoubleNested((index.getIndex()), Path.of("index.json"));
 		}
 		if(parse.hasFlag("-counts")) {
-			SimpleJsonWriter.asObject(index.getCount(), Path.of("actual/counts.json"));
+			SimpleJsonWriter.asObject(index.getCount(), Path.of("actual/counts.json")); // TODO Must be a bug if you need actual/counts.json
 		}
+		
+		/* TODO Try this for exception handling...
+		if (parse.hasFlag("-counts")) {
+			Path path = parse.getPath("-counts", Path.of("counts.json"));
+			
+			try {
+				SimpleJsonWriter.asObject(index.getCount(), path);
+			}
+			catch (IOException e) {
+				System.out.println("Unable to write the word counts to path: " + path);
+			}
+		}
+		*/
+		
+		
+		
 //		if(parse.hasFlag("-counts") && parse.getString("-counts") != null) {
 //			SimpleJsonWriter.asObject(parse.getString("counts"));
 //			
