a64746c729d06242522d72c83dd2d56d5bef873f
jnhamid <42946642+jnhamid@users.noreply.github.com>
2019-11-06 19:53:33 -0800

    Think I fixed

---
+++ Driver.java
@@ -0,0 +1,110 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collections;
+
+/**
+ * Class responsible for running this project based on the provided command-line
+ * arguments. See the README for details.
+ *
+ * @author CS 212 Software Development
+ * @author University of San Francisco
+ * @version Fall 2019
+ *
+ */
+public class Driver {
+	/**
+	 * Initializes the classes necessary based on the provided command-line
+	 * arguments. This includes (but is not limited to) how to build or search an
+	 * inverted index.
+	 *
+	 * @param args flag/value pairs used to start this program
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		InvertedIndex index = new InvertedIndex();
+		// store initial start time
+		Instant start = Instant.now();
+
+		ArgumentParser parse = new ArgumentParser(args);
+		InvertedIndexBuilder indexBuilder = new InvertedIndexBuilder(index);
+		QueryBuilder qBuilder = new QueryBuilder(index);
+		/*
+		 * This if builds the InvertedIndex if has the flag "-path"
+		 */
+		if (parse.hasFlag("-path") && parse.getPath("-path") != null) {
+			Path path = parse.getPath("-path");
+			try {
+				if (Files.exists(path)) {
+					indexBuilder.build(path);
+				}
+			} catch (IOException e) {
+				System.out.println("File is a directory");
+			}
+		}
+		/*
+		 * This if writes the output file to the path with flag "-index"
+		 */
+		if (parse.hasFlag("-index")) {
+			Path path = parse.getPath("-index", Path.of("index.json"));
+			try {
+				index.printIndex(path.toString());
+			} catch (IOException e) {
+				System.out.println("Unable to write the index to path: " + path);
+			}
+		}
+		/*
+		 * This if writes the output file to the path with flag "-counts"
+		 */
+		if (parse.hasFlag("-counts")) {
+			Path path = parse.getPath("-counts", Path.of("counts.json"));
+			try {
+				SimpleJsonWriter.asObject(index.getCount(), path);
+			} catch (IOException e) {
+				System.out.println("Unable to write the word counts to path: " + path);
+			}
+		}
+		/*
+		 * This if writes the output file to the path with flag "-query"
+		 */
+		if (parse.hasFlag("-query") && parse.getPath("-query") != null) {
+			Path qPath = parse.getPath("-query");
+			try {
+				qBuilder.makeQuery(qPath);
+				if (parse.hasFlag("-exact")) {
+					qBuilder.exactSearch();
+				} else {
+					qBuilder.partialSearch();
+				}
+			} catch (IOException e) {
+				System.out.println("Unable to read the query file" + qPath.toString());
+
+			} catch (Exception s) {
+				System.out.println("Unable to do something to the query file" + qPath.toString());
+				s.printStackTrace();
+
+			}
+		}
+		/*
+		 * This if writes the output file to the path with flag "-results"
+		 */
+		if (parse.hasFlag("-results")) {
+			Path path = parse.getPath("-results", Path.of("resutls.json"));
+			try {
+				SimpleJsonWriter.asQuery(Collections.emptyMap(), Path.of("results.json"));
+				SimpleJsonWriter.asQuery(qBuilder.getQuerySet(), path);
+			} catch (NullPointerException n) {
+				System.out.println("Cannot write null file");
+			} catch (IOException e) {
+				System.out.println("Cannot write results");
+			}
+
+		}
+		// calculate time elapsed and output
+		Duration elapsed = Duration.between(start, Instant.now());
+		double seconds = (double) elapsed.toMillis() / Duration.ofSeconds(1).toMillis();
+		System.out.printf("Elapsed: %f seconds%n", seconds);
+	}
+}
---
+++ InvertedIndexBuilder.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+import java.util.List;
+import opennlp.tools.stemmer.Stemmer;
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
+
+/**
+ * @author Jaden This is a Builder class for our InvertedIndex class.
+ */
+public class InvertedIndexBuilder {
+
+	/**
+	 * The index being built
+	 */
+	private InvertedIndex index = new InvertedIndex();
+	/**
+	 * Snowball Stemmer
+	 */
+	public static final SnowballStemmer.ALGORITHM DEFAULT = SnowballStemmer.ALGORITHM.ENGLISH;
+
+	/**
+	 * construtor
+	 * 
+	 * @param index the index being built
+	 */
+	public InvertedIndexBuilder(InvertedIndex index) {
+		this.index = index;
+	}
+
+	/**
+	 * Will build index by traversing files
+	 * 
+	 * @param path The Path that is getting checked
+	 * @throws IOException
+	 */
+	public void build(Path path) throws IOException {
+		if (Files.isRegularFile(path)) {
+			addPath(this.index, path);
+		} else {
+			for (Path newPath : getTextFiles(path)) {
+				addPath(this.index, newPath);
+			}
+		}
+	}
+
+	/**
+	 * Checks to see if path is a text file and not a directory or some trash file
+	 * 
+	 * @param path file that you are checking
+	 * @return if its a text file
+	 */
+	public static boolean isTextFile(Path path) {
+		String lowerCase = path.toString().toLowerCase();
+		return ((lowerCase.endsWith(".txt") || lowerCase.endsWith(".text")) && Files.isRegularFile(path));
+	}
+
+	/**
+	 * Gets a list of files by walking the directory or file
+	 * 
+	 * @param path path or directory that needs to be traversed
+	 * @return a list of text files
+	 * @throws IOException
+	 */
+	public static List<Path> getTextFiles(Path path) throws IOException {
+		return Files.walk(path, FileVisitOption.FOLLOW_LINKS).filter(textFile -> isTextFile(textFile))
+				.collect(Collectors.toList());
+	}
+
+	/**
+	 * Adds Path to index
+	 * 
+	 * @param index the InvertedIndex that the file is getting added to
+	 * @param file  the Path that is getting added to index.
+	 * @throws IOException
+	 */
+	public static void addPath(InvertedIndex index, Path file) throws IOException {
+		Stemmer stemmer = new SnowballStemmer(DEFAULT);
+		try (BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8);) {
+			String line = reader.readLine();
+			int i = 0;
+			String fileName = file.toString();
+			while (line != null) {
+				String[] parsed = TextParser.parse(line);
+				for (String word : parsed) {
+					String data = stemmer.stem(word).toString();
+					index.addElement(data, fileName, ++i);
+				}
+				line = reader.readLine();
+			}
+		}
+	}
+
+}
